4f55384ab5b450861de0d4a06969cf1b20a6e2ef
usr110 <m.aliabbas@gmail.com>
2015-03-03 14:41:03 +0000

    -- Added a new tab feature to the link -- Removed the unused dropdown menu

--- master/ui.R
+++ master/ui.R
@@ -8,14 +8,13 @@ fluidPage(
                  ,checkboxInput("transp_zones", label = "Transparency of zone boundaries", value = FALSE)
                  # ,sliderInput("transp_cents", label = "Transparency of centroids", min = 0, max = 1, value = 0.3)
                  ,sliderInput("transp_fast", label = "Transparency of paths", min = 0, max = 1, value = 0.7)
-                 ,selectInput("viewout", "Output to view", choices = c("Highest cycle counts", "Lowest number who cycle", "Highest potential", "Greatest extra cycling potential"))
                  # , selectInput("colour", "Colour of paths", choices = c("red", "blue", "black"))
                  , selectInput("feature", "Features", choices = c("none", "cycleparking", "collisions", "bikeshops"))
-                 
+
     ),
     mainPanel("Welcome to",
               a(href = "https://robinlovelace.shinyapps.io/fixMyPath/", "fixMyPath"),
               p("fixMyPath is a shiny app written to facilitate better bicycle path planning in Leeds, the UK and eventually the world. If you'd like to get involved, please check-out, test and contribute-to the fully reproducible code..."),
-              a(href = "https://github.com/nikolai-b/hackMyRoute/tree/master/R/fixMyPath", strong("HERE!")),
+              a(href = "https://github.com/nikolai-b/hackMyRoute/tree/master/R/fixMyPath", strong("HERE!"), target="_blank"),
               leafletOutput('map', height = 600)),
   ))
