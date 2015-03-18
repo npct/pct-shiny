1a28220a96c4e214668f4b66d58ee4e9ec93da06
Nikolai Berkoff <nikolai-b@users.noreply.github.com>
2015-03-18 22:22:14 +0000

    DRY and refactor

--- master/server.R
+++ master/server.R
@@ -3,26 +3,12 @@ lapply(pkgs, library, character.only = TRUE)
 
 sapply('./cyclestreet.R', FUN=source)
 
-bb_poly <- function(bounds){
-  lat <- c(bounds$west , bounds$east, bounds$east, bounds$west )
-  lng <- c(bounds$north, bounds$north, bounds$south, bounds$south)
-  c1 <- cbind(lat, lng)
-  r1 <- rbind(c1, c1[1, ])
-  SpatialPolygons(list(Polygons(list(Polygon(r1)), 'bb')), proj4string=CRS("+init=epsg:4326 +proj=longlat"))
-}
-
-# Load data
-rfast <- readRDS("../data/manchester/rf.Rds")
-rquiet <- readRDS("../data/manchester/rq.Rds")
-l <- readRDS("../data/manchester/l.Rds")
-zones <- readRDS("../data/manchester/z.Rds")
-flow <- l@data
-
-journeyLabel <- function(distance, percentage, route){
-  sprintf("<dl><dt>Distance </dt><dd>%s km</dd><dt>Journeys by bike</dt><dd>%s%%</dd><dt>Type of Route</dt><dd>%s</dd></dl>", distance, percentage, route)
-}
-
 routePopup <- function(data){
+  sprintf('<dl><dt>Distance </dt><dd>%s km</dd><dt>Journeys by bike</dt><dd>%s%%</dd><dt>Type of Route</dt><dd>%s</dd></dl>',
+          round(data$length / 1000, 1), round(data$clc * 10, 2), data$plan[1])
+}
+
+straightPopup <- function(data){
   sprintf('
   <table>
     <tbody>
@@ -52,23 +38,50 @@ routePopup <- function(data){
       </tr>
     </tbody>
   </table>', data$All, data$Bicycle, round(data$clc * 100, 1), round(data$plc * 100, 1), round(data$ecp, 1), round(data$dist, 1)
-          )}
-
-sort_lines <- function(lines, scenario, nos, bounds){
-  poly <- bb_poly(bounds)
-  proj4string(poly)=CRS("+init=epsg:4326 +proj=longlat")
-  poly <- spTransform(poly, CRS(proj4string(lines)))
-  keep <- gContains( poly, lines,byid=TRUE ) | gOverlaps( poly, lines,byid=TRUE )
-  if(all(!keep)){
-    return(NULL)
-  }
-  l_in_bb <- lines[drop(keep), ]
-  l_in_bb[ tail(order(l_in_bb[[scenario]]), nos), ]
+  )
 }
 
+# Load data
+rfast <- readRDS("../data/manchester/rf.Rds")
+rquiet <- readRDS("../data/manchester/rq.Rds")
+l <- readRDS("../data/manchester/l.Rds")
+zones <- readRDS("../data/manchester/z.Rds")
+flow <- l@data
+cents <- coordinates(zones)
+cents <- SpatialPointsDataFrame(cents, data = zones@data, match.ID = F)
+
 shinyServer(function(input, output){
-  cents <- coordinates(zones)
-  cents <- SpatialPointsDataFrame(cents, data = zones@data, match.ID = F)
+  sortLines <- function(lines, scenario, nos){
+    poly <- bbPoly()
+    poly <- spTransform(poly, CRS(proj4string(lines)))
+    keep <- gIntersects( poly, lines,byid=TRUE ) | gOverlaps( poly, lines,byid=TRUE )
+    if(all(!keep)){
+      return(NULL)
+    }
+    linesInBb <- lines[drop(keep), ]
+    linesInBb[ tail(order(linesInBb[[scenario]]), nos), ]
+  }
+
+  bbPoly <- reactive({
+    lat <- c(input$map_bounds$west , input$map_bounds$east, input$map_bounds$east, input$map_bounds$west )
+    lng <- c(input$map_bounds$north, input$map_bounds$north, input$map_bounds$south, input$map_bounds$south)
+    c1 <- cbind(lat, lng)
+    r1 <- rbind(c1, c1[1, ])
+    poly <- SpatialPolygons(list(Polygons(list(Polygon(r1)), 'bb')), proj4string=CRS("+init=epsg:4326 +proj=longlat"))
+    proj4string(poly)=CRS("+init=epsg:4326 +proj=longlat")
+    return(poly)
+  })
+
+  sortAndPlot <- function(m, lines, attr, nos, color, popupFn){
+    sorted_l <- sortLines(lines, attr, nos)
+    if(is.null(sorted_l))
+      .
+    else
+      addPolylines(m, data = sorted_l, color = color
+                   # Sequence in descending order
+                   , weight = seq(from = 1.2, to = 5, length = nos)
+                   , popup = popupFn(sorted_l) )
+  }
 
   map <- leaflet() %>%
     addTiles(urlTemplate = "http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png")
@@ -87,31 +100,19 @@ shinyServer(function(input, output){
                                } %>%
                                {
                                  if (input$line_type == 'straight'){
-                                   sorted_l <- sort_lines(l, input$line_attr, as.numeric(input$nos_lines), input$map_bounds)
-                                   if(is.null(sorted_l))
-                                     .
-                                   else
-                                     addPolylines(., data = sorted_l, color = 'blue'
-                                                  # Sequence in descending order
-                                                  , weight = seq(from = 6, to = 3, length = as.numeric(input$nos_lines))
-                                                  , popup = routePopup(sorted_l) )
+                                   sortAndPlot(., l, input$line_attr, as.numeric(input$nos_lines),
+                                               "blue", straightPopup)
+
                                  }else
                                    .
                                }%>%
                                {
-                                 if (input$line_type == 'route' && input$nos_lines != 0){
-                                   sorted_fast  <- sort_lines(rfast, input$line_attr, as.numeric(input$nos_lines), input$map_bounds)
-                                   sorted_quiet <- sort_lines(rquiet, input$line_attr, as.numeric(input$nos_lines), input$map_bounds)
-                                   addPolylines(., data = sorted_fast, color = "red"
-                                                , weight = seq(4, 3, length = as.numeric(input$nos_lines))
-                                     ,opacity = 0.9
-                                                , popup = journeyLabel(round(sorted_fast$length / 1000, 1), round(sorted_fast$clc * 10, 2), "Fast")
-                                   ) %>%
-                                   addPolylines(data = sort_lines(sorted_quiet, input$line_attr, input$nos_lines, input$map_bounds), color = "green",
-                                                , weight = seq(4, 3, length = as.numeric(input$nos_lines))
-                                     ,opacity = 0.9
-                                                , popup = journeyLabel(round(sorted_quiet$length / 1000, 1), round(sorted_quiet$clc * 10, 2), "Quiet")
-                                   )
+                                 if (input$line_type == 'route'){
+                                   sortAndPlot(., rfast, input$line_attr, as.numeric(input$nos_lines),
+                                               "red", routePopup) %>%
+                                     sortAndPlot(., rquiet, input$line_attr, as.numeric(input$nos_lines),
+                                                 "green", routePopup)
+
                                  }else
                                    .
                                }%>%
