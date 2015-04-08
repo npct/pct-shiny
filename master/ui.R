5d6c742202e0457ccb4fc83a79ea612fe82213df
usr110 <m.aliabbas@gmail.com>
2015-04-08 18:06:34 +0100

    Change in route name

--- master/ui.R
+++ master/ui.R
@@ -9,7 +9,7 @@ scenarios <- c("Baseline" =           "base"
 line_types <- c("None" = "none"
                 ,"Straight Lines" = "straight"
                 ,"Direct Route" = "d_route"
-                ,"Direct & Quietest Routes" = "route")
+                ,"Direct & Quieter Routes" = "route")
 
 attrs <- c("Current Level Cycling (CLC)" =       "clc"
            ,"Potential Level of Cycling (PLC)" = "plc"
