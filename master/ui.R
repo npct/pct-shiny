e1b4788d5d011ce39c053e2baa078f3b1abf6b49
Nikolai Berkoff <nikolai-b@users.noreply.github.com>
2015-04-29 20:13:25 +0100

    Revert "Zone boundaries are set to be invisible whenever any route (fast or quiet) or line (straight) is selected by the user."
    This reverts commit 30c3648e56d60ac8664b4f61f817a6da25a685dd.

--- master/ui.R
+++ master/ui.R
@@ -53,22 +53,9 @@ shinyUI(navbarPage("Infrastructure planning tool", id="nav"
                                                  ,sliderInput("nos_lines", label = "Flows to show (top n)", 1, 20, value = 5)
                                )
 
-
                              )
-
-                             ,absolutePanel(
-                                cursor = "default"
-                                ,id = "cursor"
-                                ,class = "panel panel-default"
-                                ,fixed = TRUE
-                                ,bottom = 0
-                                ,left = 15
-                                ,width = "65%"
-                                ,height = "auto"
-                                ,style = "opacity: 0.9"
-                                ,bsAlert("alert")
-                             )
-
+                             ,bsTooltip(id = "legend", title = "Scenario-specific quartiles</br>of Cycling Level",
+                                        placement = "centre", trigger = "hover")
                              ,absolutePanel(
                                cursor = "default"
                                ,id = "legend"
@@ -81,13 +68,9 @@ shinyUI(navbarPage("Infrastructure planning tool", id="nav"
                                ,plotOutput("legendCyclingPotential", width = "100%")
                                )
 
-                              ,bsTooltip(id = "legend", title = "Scenario-specific quartiles</br>of Cycling Level",
-                                         placement = "centre", trigger = "hover")
-                  )
+                   )
                    ,tabPanel("Help"
                     ,helpText("This tab contains information and screenshots on how to use the tool better.")
                     ,includeHTML("help.html")
                    )
-
-
 ))
