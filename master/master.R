fb08abd94fdad41db97affabdac6dbbbdba6cd7b
usr110 <m.aliabbas@gmail.com>
2015-05-07 13:45:24 +0100

    Shorten the legend label to fit to a single line

--- master/master.R
+++ master/master.R
@@ -182,17 +182,17 @@ shinyServer(function(input, output, session){
     zone_col <- getColourRamp(zcols, m)
 
     # Set a full form of the scenario as a label
-    ylabel <- "Observed Level Cycling (OLC): Cycle Commuters"
+    ylabel <- "Observed Level Cycling (OLC): Commuters"
     if (zoneAttr() == "slc")
-      ylabel <- "Scenario-based Level of Cycling (SLC): Cycle Commuters"
+      ylabel <- "Scenario-based Level of Cycling (SLC): Commuters"
     else if (zoneAttr() == "sic")
-      ylabel <- "Scenario-based Increase in Cycling (SIC): Cycle Commuters"
+      ylabel <- "Scenario-based Increase in Cycling (SIC): Commuters"
 
     # Set the labelling of Y-axis to bold
     par(font.lab = 2)
+
     # Barplot the data in vertical manner
     barplot(height = rep(1, 4), names.arg = round(matrix(m, nrow=4,ncol=1)),
             col = zone_col, horiz=TRUE, xlab = "", ylab = ylabel, space = 0, axes = FALSE)
-
   })
 })
