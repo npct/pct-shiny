60b092eef6a547bf8235ca26b0cc0f9b961f96f3
Robinlovelace <rob00x@gmail.com>
2015-06-05 14:17:45 +0100

    added temp dir

---
+++ temporary/extra.js
@@ -0,0 +1,25 @@
+$( window ).load(function() {
+  var togglePanel = function(panelId, link){
+    var panel = $(panelId)
+    panel.toggle();
+    if(panel.is(":visible")){
+      $(link).html('<span class="glyphicon glyphicon-circle-arrow-up">Hide</span>');
+    }else{
+      $(link).html('<span class="glyphicon glyphicon-circle-arrow-down">Show</span>');
+    }
+  };
+  $('#togglePanel').click(function(){ togglePanel('#input_panel', this) });
+  $('#toggleLegend').click(function(){ togglePanel('#zone_legend', this) });
+
+  var initMap = function(){
+    if($(map).data('leaflet-map')){
+      // lMap is the leaflet map object see http://leafletjs.com/reference.html
+      var lMap = $(map).data('leaflet-map');
+      L.control.scale().addTo(lMap);
+    }
+    else {
+      setTimeout(initMap, 100);
+    }
+  }
+  initMap();
+});
