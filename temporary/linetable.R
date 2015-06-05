70944b2a19a9c416abe62838c44be5980f81a28c
Robinlovelace <rob00x@gmail.com>
2015-06-05 14:33:10 +0100

    reverted tempdir addition

--- temporary/linetable.R
+++
@@ -1,2 +0,0 @@
-# Create lines table to display in popup
-kable(data.frame(Variable = c("Total commutes", "N. Cyclists", "% Cyclists", "CLC", "PLC", "ECP", "Euclidean Distance (km)", "Fastest distance (km)", "Longest Distance (km)"), Value = rep("%s", 9)), format = "html")
--- temporary/server.R
+++
@@ -1,2 +0,0 @@
-data_dir <- "../master/pct-data/coventry"
-source("master.R", local = T, chdir = T)
