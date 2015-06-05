91ea8d1f44f39809fcdc1ffa5d74a71113fdc76c
Robinlovelace <rob00x@gmail.com>
2015-06-05 14:54:05 +0100

    Revert "remove attempt to install packages automatically"
    This reverts commit f9525161af2d1141ec289c4cce6156795d15f45c.

--- master/master.R
+++ master/master.R
@@ -2,12 +2,16 @@
 # Setup #
 # # # # #
 
-# use install.packages() install these
-cranPkgs <- c("shiny", "RColorBrewer", "httr", "rgdal", "rgeos")
-# use devtools::install_github("rstudio/PkgName")
-devPkgs <- c("leaflet", "DT")
+# use install.packages() or devtools::install_github() to install these
+pkgs <- c("shiny", "leaflet", "RColorBrewer", "httr", "rgdal", "rgeos", "DT")
 
-lapply(c(cranPkgs, devPkgs), library, character.only = TRUE)
+# Install packages if they are not already installed
+if (length(setdiff(pkgs, rownames(installed.packages()))) > 0) {
+  install.packages(setdiff(pkgs, rownames(installed.packages())))
+}
+
+# Install all packages
+lapply(pkgs, library, character.only = TRUE)
 
 # Colours
 zcols <- c("darkslategrey", "yellow")
