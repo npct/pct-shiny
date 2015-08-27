2a3e79f21dc567457d0458e93d07f50450eff19a
usr110 <m.aliabbas@gmail.com>
2015-08-27 12:55:47 +0100

    Removed unnecessary code

--- master/master.R
+++ master/master.R
@@ -188,8 +188,7 @@ shinyServer(function(input, output, session){
       addTiles(urlTemplate = "http://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}")  %>%
       addTiles(urlTemplate = mapTileUrl(),
                attribution = '<a target="_blank" href="http://shiny.rstudio.com/">Shiny</a> | Route data from <a target="_blank" href ="https://www.cyclestreets.net">CycleStreets</a>',
-               options=tileOptions(opacity = 1, reuseTiles = T)) %>%
-
+               options=tileOptions(opacity = 1, reuseTiles = T))
     %>%{
       ## Add polygons (of MSOA boundaries)
       if(plotZones())
