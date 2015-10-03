131e64deb7dd0e9de407c1c8b068365f026cec4b
Robinlovelace <rob00x@gmail.com>
2015-10-03 08:29:08 +0100

    Add Barnet and Barnesley

--- Barnet/server.R
+++ Barnet/server.R
@@ -1,3 +1,2 @@
-startingCity <- "norfolk"
+startingCity <- "Barnet"
 source("../master/master.R", local = T, chdir = T)
-
--- Barnsley/server.R
+++ Barnsley/server.R
@@ -1,3 +1,2 @@
-startingCity <- "norfolk"
+startingCity <- "Barnsley"
 source("../master/master.R", local = T, chdir = T)
-
