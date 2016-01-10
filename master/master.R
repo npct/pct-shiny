7f8b0c8fd834577ee31b4f82bace94dc37343a04
Nikolai B <nikolai-b@users.noreply.github.com>
2016-01-10 20:58:07 +0000

    Change route network max slider value to 50

--- master/master.R
+++ master/master.R
@@ -165,9 +165,9 @@ shinyServer(function(input, output, session){
       )
     }
     if(input$line_type == 'rnet')
-      updateSelectInput(session, inputId = "nos_lines", label = "Percent (%) of Network")
+      updateSliderInput(session, inputId = "nos_lines", max= 50, label = "Percent (%) of Network")
     else
-      updateSelectInput(session, inputId = "nos_lines", label = "Number of Lines")
+      updateSliderInput(session, inputId = "nos_lines", max= 100, label = "Number of Lines")
 
     # Needed to force lines to be redrawn when scenario, zone or base map changes
     paste(input$scenario, input$map_base)
