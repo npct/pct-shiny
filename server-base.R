30f28423c2e4b81543c54b9a79dd7df71a916edc
usr110 <m.aliabbas@gmail.com>
2016-03-08 10:34:05 +0000

    Scaled down the route slider from 100 to 50

--- server-base.R
+++ server-base.R
@@ -171,7 +171,7 @@ shinyServer(function(input, output, session){
       )
     }
     if(input$line_type == 'rnet')
-      updateSliderInput(session, inputId = "nos_lines", min = 25, step = 25, label = "Percent (%) of Network")
+      updateSliderInput(session, inputId = "nos_lines", min = 25, max= 50, step = 25, label = "Percent (%) of Network")
     else
       updateSliderInput(session, inputId = "nos_lines", max= 100, step = 5,  label = "Number of Lines")
 
