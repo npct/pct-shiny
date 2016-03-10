bbc211f1c8827cfabbecd1a2698cb7efc29cc8c9
usr110 <m.aliabbas@gmail.com>
2016-03-10 15:51:55 +0000

    Uncomment code to get the updated pct-data on non-production computers.

--- server-base.R
+++ server-base.R
@@ -30,9 +30,9 @@ cranPkgs <- c("shiny", "RColorBrewer", "httr", "rgdal", "rgeos", "leaflet", "DT"
 onProduction <- grepl('^/var/shiny/pct-shiny', getwd())
 
 # Run the following lines to check out the current version of the data (see sha)
-# if(!onProduction){
-#   source(file.path(shinyRoot, "scripts", "init.R"), local = T)
-# }
+if(!onProduction){
+  source(file.path(shinyRoot, "scripts", "init.R"), local = T)
+}
 repo_sha <- as.character(readLines(file.path(shinyRoot, "data_sha")))
 lapply(c(cranPkgs), library, character.only = TRUE)
 
