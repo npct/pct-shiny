7d46fc975091ee73dfc6e9765cf4930d0306cb31
Nikolai B <nikolai-b@users.noreply.github.com>
2016-03-30 22:08:29 +0100

    Re-add init code so data updates automatically
    Partial revert of 7894b5e
    To avoid data updating simply delete the
    data_sha file, Windows users need to install git,
    they always have, our workflow requires it.

--- scripts/init.R
+++ scripts/init.R
@@ -1,28 +1,23 @@
 # Clone the data repo if it do not exist
-
 if(!dir.exists(dataDirRoot)) {
   system2('git', args=c('clone', '--depth=1',
                         'https://github.com/npct/pct-data.git', dataDirRoot))
+} else { # Update the data repo
+  print("Fetching data")
+  system2('git', c("--git-dir=", file.path(dataDirRoot, ".git"), "--work-tree=",
+                   dataDirRoot, "fetch"), wait = F)
 }
-
-# Update the data repo
-if(dir.exists(dataDirRoot)){
-  old <- setwd(dataDirRoot)
-  system2('git', "fetch", wait = F)
-  setwd(old)
-}
-
-data_sha <- readLines(file.path(shinyRoot, "data_sha"))
 
 installed <- cranPkgs %in% installed.packages()
 # install packages that are missing
 if(length(cranPkgs[!installed]) > 0) install.packages(cranPkgs[!installed], repos='https://cran.rstudio.com/')
 
-# The following lines checkout the version of the data saved in data_sha.
-# Uncomment to auto checkout pct-data.
-# warning("Checking out a the version of the data saved in the data sha")
-# system2("git", c("--git-dir", file.path(dataDirRoot, ".git"), "--work-tree",
-#                  dataDirRoot, "checkout", data_sha), wait=T)
-
-# Run this next line from the root directory to save update the 'repo_sha' file used by pct-shiny
-# system2("git", c("rev-parse", "--short", "HEAD", ">", file.path("repo_sha")), wait=T)
+# Delete the data_sha file to stop auto checkout
+data_sha_file <- file.path(shinyRoot, "data_sha")
+if (file.exists(data_sha_file)){
+  data_sha <- readLines(data_sha_file)
+  warning("Checking out a the version of the data saved in the data sha")
+  system2("git", c("--git-dir", file.path(dataDirRoot, ".git"), "--work-tree",
+                   dataDirRoot, "checkout", data_sha), wait=T)
+}
+system2("git", c("rev-parse", "--short", "HEAD", ">", file.path(shinyRoot, "repo_sha")), wait=T)
