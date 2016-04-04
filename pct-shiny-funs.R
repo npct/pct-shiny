fa6c22e9d890e6da880083e4628863fb38fc783f
usr110 <m.aliabbas@gmail.com>
2016-04-04 12:19:01 +0100

    Introduce a combined label for 'change in deaths per year' for fast route popups

--- pct-shiny-funs.R
+++ pct-shiny-funs.R
@@ -207,12 +207,10 @@ routePopup <- function(data, scenario){
       <td>", round(data[[dataFilter(scenario, "sid")]]), "</td>
     </tr>
     <tr>
-      <td> Deaths avoided (per 1000/yr): &nbsp; </td>
-      <td>", round(1000 * data[[dataFilter(scenario, "sideath_heat")]]), "</td>
-    </tr>
-    <tr>
-      <td> Lives saved (&pound;1000/yr): </td>
-      <td>", round(data[[dataFilter(scenario, "sivalue_heat")]] / 1000), "</td>
+      <td> Change in deaths/yr: &nbsp; </td>
+      <td>", round(data[[dataFilter(scenario, "sideath_heat")]], 2), " (&pound;" ,
+          round(data[[dataFilter(scenario, "sivalue_heat")]]), ")
+    </td>
     </tr>
     <tr>
       <td> Change in CO<sub>2</sub>e (t/yr):</td>
