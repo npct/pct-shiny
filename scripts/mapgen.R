858464954b87ce695cb78c0d89c07d4a62f700d5
Nikolai B <nikolai-b@users.noreply.github.com>
2016-07-24 10:16:48 +0100

    Test commit II

--- scripts/mapgen.R
+++ scripts/mapgen.R
@@ -89,12 +89,3 @@ m
 old = setwd("regions_www/")
 saveWidget(m, file = "map.html")
 setwd(old)
-# # V1: builds all zones for a single geography
-# pkgs <- c("leaflet", "htmlwidgets", "geojsonio")
-# lapply(pkgs, library, character.only = T)
-# downloader::download("https://github.com/npct/pct-bigdata/raw/master/national/regions.geojson",
-#                      destfile = "regions.geojson")
-# regions <- geojson_read("regions.geojson", what = "sp")
-# file.remove("regions.geojson")
-# names(regions)
-# m <- leaflet() %>% addTiles() %>% addPolygons(data = regions, popup = regions$url_text)
