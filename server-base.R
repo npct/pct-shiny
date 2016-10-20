546ecdf9bc782029ac74ea9639766a3b443b3a04
robinlovelace <rob00x@gmail.com>
2016-10-20 23:27:09 +0100

    Change hilliness basemap - fix npct/pct#27

--- server-base.R
+++ server-base.R
@@ -428,7 +428,7 @@ shinyServer(function(input, output, session){
            'satellite' = "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
            'IMD' =  "http://tiles.oobrien.com/imd2015_eng/{z}/{x}/{y}.png",
            'opencyclemap' = "https://c.tile.thunderforest.com/cycle/{z}/{x}/{y}.png",
-           'hilliness' = "http://{s}.tiles.wmflabs.org/hillshading/{z}/{x}/{y}.png"
+           'hilliness' = "http://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}"
     )
   })
 
