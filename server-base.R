4c412960f6f8a07f9cee5962f300fbcd15b86cf7
Nikolai B <nikolai-b@users.noreply.github.com>
2016-12-13 18:42:24 +0000

    Revert attempted fix of download data update
    This reverts commit 42549cfdffe013d6f1e3ca6c7f44a34a5fb634a1.
    Heads up @usr110, I don't believe theses are needed

--- server-base.R
+++ server-base.R
@@ -652,8 +652,6 @@ shinyServer(function(input, output, session){
   })
 
   observe({
-    region$current
-    region$data_dir
     region$repopulate_region
 
     output$download_l_csv <- downloadHandler(
