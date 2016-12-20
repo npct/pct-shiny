3a1cdc1789cbce324b55f6d826c4e48e6ccb532d
usr110 <m.aliabbas@gmail.com>
2016-12-20 16:36:44 +0000

    Introduce a new variable which calculates `all-trips` value for the new region. Fix #500.

--- regions_www/msoa/server.R
+++ regions_www/msoa/server.R
@@ -267,8 +267,11 @@ shinyServer(function(input, output, session){
   observe({
     if(file.exists(file.path(region$data_dir, 'isolated'))) return()
     new_region <- find_region(region$current)
+    new_region_all_trips <- T
+    if (!is.null(new_region))
+      new_region_all_trips <- dir.exists(file.path(data_dir_root, new_region , 'all-trips'))
     # Check if the new_region is not null, and contains 'all-trips' subfolder
-    new_data_dir <- ifelse ((!is.null(new_region) && region$all_trips && showing_all_trips()),
+    new_data_dir <- ifelse ((!is.null(new_region) && new_region_all_trips && showing_all_trips()),
                             file.path(data_dir_root, new_region, 'all-trips'),
                             file.path(data_dir_root, new_region))
 
