2e761bb42726a1994abe1e7c8b94d6449af279e6
Nikolai B <nikolai-b@users.noreply.github.com>
2017-01-06 16:28:59 +0000

    Add sudo

--- scripts/init_server/generate-server.sh
+++ scripts/init_server/generate-server.sh
@@ -10,14 +10,14 @@ gpg -a --export 51716619E084DAB9 > cran.asc
 apt-key add cran.asc
 rm cran.asc
 echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" | tee -a /etc/apt/sources.list
-apt-get -yy install r-recommended git gdebi-core
+sudo apt-get -yy install r-recommended git gdebi-core
 
 # Install shiny
- R -e "install.packages('shiny', repos = 'https://cran.rstudio.com/')"
+R -e "install.packages('shiny', repos = 'https://cran.rstudio.com/')"
 
 wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.5.1.834-amd64.deb
 gdebi shiny-server-1.5.1.834-amd64.deb
-apt-get -yy install libssl libgdal-dev gdal-bin libproj-dev
+sudo apt-get -yy install libssl libgdal-dev gdal-bin libproj-dev
 
 R -e "install.packages(c('shiny', 'rgdal', 'rgeos', 'leaflet', 'DT', 'shinyjs', 'dplyr', 'readr'), repos='https://cran.rstudio.com/')"
 
