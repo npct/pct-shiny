9f717b906228a01080c21105c17c494d22e486c2
Nikolai B <nikolai-b@users.noreply.github.com>
2017-01-08 11:48:16 +0000

    Remove old script, add missing sudo

--- scripts/init_server/generate-server.sh
+++ scripts/init_server/generate-server.sh
@@ -3,13 +3,14 @@
 # Building on Server.md
 
 GIT_DIR=${1##*/}
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # CRAN R statistical packages
 gpg --keyserver pgp.mit.edu --recv-key 51716619E084DAB9
 gpg -a --export 51716619E084DAB9 > cran.asc
-apt-key add cran.asc
+sudo apt-key add cran.asc
 rm cran.asc
-echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" | tee -a /etc/apt/sources.list
+echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get -yy install r-recommended git gdebi-core
 
@@ -17,15 +18,17 @@ sudo apt-get -yy install r-recommended git gdebi-core
 R -e "install.packages('shiny', repos = 'https://cran.rstudio.com/')"
 
 wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.5.1.834-amd64.deb
-gdebi shiny-server-1.5.1.834-amd64.deb
-sudo apt-get -yy install libssl libgdal-dev gdal-bin libproj-dev
+sudo gdebi shiny-server-1.5.1.834-amd64.deb
+sudo apt-get -yy install libcurl4-openssl-dev libgdal-dev gdal-bin libproj-dev libgeos-dev
 
-R -e "install.packages(c('shiny', 'rgdal', 'rgeos', 'leaflet', 'DT', 'shinyjs', 'dplyr', 'readr'), repos='https://cran.rstudio.com/')"
+sudo R -e "install.packages(c('shiny', 'rgdal', 'rgeos', 'leaflet', 'DT', 'shinyjs', 'dplyr', 'readr'), repos='https://cran.rstudio.com/')"
 
-mkdir /var/shiny
+sudo mkdir /var/shiny
 sudo chown -R shiny:shiny /var/shiny
 
+cd ${HOME}
 git clone --bare $1
 
-cp -f generate-server/post-update ${GIT_DIR}/hooks
-sudo cp -f generate-server/shiny-server.conf /etc/shiny-server/shiny-server.conf
+cp -f ${SCRIPT_DIR}/generate-server/post-update ${GIT_DIR}/hooks
+sudo cp -f ${SCRIPT_DIR}/generate-server/shiny-server.conf /etc/shiny-server/shiny-server.conf
+sudo systemctl restart shiny-server
