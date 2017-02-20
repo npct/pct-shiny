30fc2457fa524f986ad43ec854c9b2a46a54f45a
Malcolm Morgan <malcolmmorgan02@hotmail.com>
2017-02-20 14:55:41 +0000

    Change base map on front page
    Now same as rest of the maps

--- regions_www/www/front_page/map.js
+++ regions_www/www/front_page/map.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
   $.getJSON("/regions.geojson").done(function(statesData) {
     var map = L.map('map').setView([ 53, -0.4], 6);
 
-    L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
       maxZoom: 18,
       attribution: '<a href="http://leafletjs.com" target = "_blank">Leaflet</a> | ' +
       'Map data &copy; <a href="http://openstreetmap.org" target = "_blank">OpenStreetMap</a> contributors | ' +
