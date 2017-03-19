b4edb9dd51a44488ece38ce7064ec9c9dc68970b
Nikolai B <nikolai-b@users.noreply.github.com>
2017-03-19 08:33:48 +0000

    Add test banner
    Fixes #553

--- regions_www/m/ui.R
+++ regions_www/m/ui.R
@@ -48,6 +48,7 @@ map_base_attrs <- c("Roadmap (Black & White)" = "roadmap",
                     )
 
 on_server <- grepl('^/var/shiny/pct-shiny', getwd())
+production_branch <- grepl("npt\\d*$", Sys.info()["nodename"])
 
 shinyUI(
   navbarPage(
@@ -64,6 +65,9 @@ shinyUI(
           includeCSS("../www/stylesheet.css"),
           includeHTML(file.path("..", "favicon.html"))
         ),
+        if(!production_branch){
+          includeHTML(file.path("..", "test-banner.html"))
+        },
         br(),
         leafletOutput("map", width="100%", height="95%"),
         absolutePanel(
