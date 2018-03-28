28f08852ac73331cd5d2685fd0ed2ddf3f70010b
usr110 <m.aliabbas@gmail.com>
2018-03-28 14:42:21 +0100

    Make ui variable which has shinyUI insside a function with request as param. This is required to save state.

--- regions_www/m/ui.R
+++ regions_www/m/ui.R
@@ -61,147 +61,149 @@ map_base_attrs <- c(
 on_server <- grepl('^/var/shiny/pct-shiny', getwd())
 production_branch <- grepl("npt\\d*$", Sys.info()["nodename"])
 
-shinyUI(
-  navbarPage(
-    title = "Propensity to Cycle Tool",
-    id = "nav",
-    tabPanel(
-      "Map",
-      useShinyjs(),
-      div(
-        class = "outer",
-        tags$head(
-          if(on_server) includeScript("../www/assets/google-analytics.js"),
-          includeScript("../www/assets/extra.js"),
-          includeCSS("../www/stylesheet.css"),
-          includeHTML(file.path("..", "favicon.html"))
-        ),
-        includeHTML(file.path("..", "www", "test-banner.html")),
-        br(),
-        div(id="loading", "Loading&#8230;"),
-        leafletOutput("map", width = "100%", height = "95%"),
-        absolutePanel(
-          id = "controls",
-          class = "panel panel-default",
-          fixed = TRUE,
-          top = 110,
-          right = 20,
-          width = 180,
-          height = "auto",
-          style = "opacity: 0.9",
-          tags$div(title = "Show/Hide Panel",
-                   a(
-                     id = "toggle_panel",
-                     style = "font-size: 80%",
-                     span(class = "glyphicon glyphicon-circle-arrow-up", "Hide")
-                   )),
-          div(
-            id = "input_panel",
-            if (!production_branch) {
-              tags$div(title = "Trip purpose:",
-                       selectInput("purpose", "Trip purpose:", purposes, selectize = F))
-            },
-            tags$div(title = "Geography:",
+ui <- function(request){
+  shinyUI(
+    navbarPage(
+      title = "Propensity to Cycle Tool",
+      id = "nav",
+      tabPanel(
+        "Map",
+        useShinyjs(),
+        div(
+          class = "outer",
+          tags$head(
+            if(on_server) includeScript("../www/assets/google-analytics.js"),
+            includeScript("../www/assets/extra.js"),
+            includeCSS("../www/stylesheet.css"),
+            includeHTML(file.path("..", "favicon.html"))
+          ),
+          includeHTML(file.path("..", "www", "test-banner.html")),
+          br(),
+          div(id="loading", "Loading&#8230;"),
+          leafletOutput("map", width = "100%", height = "95%"),
+          absolutePanel(
+            id = "controls",
+            class = "panel panel-default",
+            fixed = TRUE,
+            top = 110,
+            right = 20,
+            width = 180,
+            height = "auto",
+            style = "opacity: 0.9",
+            tags$div(title = "Show/Hide Panel",
+                     a(
+                       id = "toggle_panel",
+                       style = "font-size: 80%",
+                       span(class = "glyphicon glyphicon-circle-arrow-up", "Hide")
+                     )),
+            div(
+              id = "input_panel",
+              if (!production_branch) {
+                tags$div(title = "Trip purpose:",
+                         selectInput("purpose", "Trip purpose:", purposes, selectize = F))
+              },
+              tags$div(title = "Geography:",
                        selectInput("geography", "Geography:", geographies, selectize = F)
-            ),
-            tags$div(
-              title = "Scenario (see manual)",
-              selectInput("scenario", "Scenario:", scenarios, selectize = F)
-            ),
-            tags$div(
-              title = "Shows the cycling flow between the centres of zones",
-              selectInput(
-                "line_type",
-                "Cycling Flows:",
-                line_types,
-                selected = "none",
-                selectize = F
-              )
-            ),
-            tags$div(title = "Shows the cycling flow between the centres of zones",
-                     checkboxInput("show_zones", "Show Zones", value = T)),
-
-            conditionalPanel(
-              condition = "input.line_type != 'none' && input.line_type != 'lsoa_base_map' && input.line_type != 'route_network'",
-              tags$div(title = "Untick to update lines when you move the map",
-                       checkboxInput("freeze", "Freeze Lines", value = F))
-            ),
-            conditionalPanel(
-              condition = "input.line_type != 'none' && input.line_type != 'lsoa_base_map'",
+              ),
               tags$div(
-                title = "Number of lines to show",
-                sliderInput(
-                  "nos_lines",
-                  label = "Top N Lines (most cycled)",
-                  1,
-                  200,
-                  value = 30,
-                  ticks = F
+                title = "Scenario (see manual)",
+                selectInput("scenario", "Scenario:", scenarios, selectize = F)
+              ),
+              tags$div(
+                title = "Shows the cycling flow between the centres of zones",
+                selectInput(
+                  "line_type",
+                  "Cycling Flows:",
+                  line_types,
+                  selected = "none",
+                  selectize = F
                 )
               ),
+              tags$div(title = "Shows the cycling flow between the centres of zones",
+                       checkboxInput("show_zones", "Show Zones", value = T)),
+
               conditionalPanel(
-                condition = "input.line_type != 'route_network' && input.scenario != 'olc'",
+                condition = "input.line_type != 'none' && input.line_type != 'lsoa_base_map' && input.line_type != 'route_network'",
+                tags$div(title = "Untick to update lines when you move the map",
+                         checkboxInput("freeze", "Freeze Lines", value = F))
+              ),
+              conditionalPanel(
+                condition = "input.line_type != 'none' && input.line_type != 'lsoa_base_map'",
                 tags$div(
-                  title = "Order the top flows by",
-                  selectInput(
-                    "line_order",
-                    "Order lines by",
-                    line_order,
-                    selected = "slc",
-                    selectize = F
+                  title = "Number of lines to show",
+                  sliderInput(
+                    "nos_lines",
+                    label = "Top N Lines (most cycled)",
+                    1,
+                    200,
+                    value = 30,
+                    ticks = F
+                  )
+                ),
+                conditionalPanel(
+                  condition = "input.line_type != 'route_network' && input.scenario != 'olc'",
+                  tags$div(
+                    title = "Order the top flows by",
+                    selectInput(
+                      "line_order",
+                      "Order lines by",
+                      line_order,
+                      selected = "slc",
+                      selectize = F
+                    )
                   )
                 )
+              ),
+              tags$div(
+                title = "Change base of the map",
+                selectInput("map_base", "Map Base:", map_base_attrs, selectize = F)
               )
-            ),
-            tags$div(
-              title = "Change base of the map",
-              selectInput("map_base", "Map Base:", map_base_attrs, selectize = F)
             )
-          )
-        ),
-        conditionalPanel(
-          condition = "input.map_base == 'IMD'",
-          absolutePanel(
-            cursor = "auto",
-            id = "legend",
-            class = "panel panel-default",
-            bottom = 235,
-            left = 5,
-            height = 20,
-            width = 225,
-            draggable = TRUE,
-            style = "opacity: 0.7",
-            tags$div(
-              title = "Show/Hide map legend",
-              a(
-                id = "toggle_imd_legend",
-                style = "font-size: 80%",
-                span(class = "glyphicon glyphicon-circle-arrow-up", "Hide")
+          ),
+          conditionalPanel(
+            condition = "input.map_base == 'IMD'",
+            absolutePanel(
+              cursor = "auto",
+              id = "legend",
+              class = "panel panel-default",
+              bottom = 235,
+              left = 5,
+              height = 20,
+              width = 225,
+              draggable = TRUE,
+              style = "opacity: 0.7",
+              tags$div(
+                title = "Show/Hide map legend",
+                a(
+                  id = "toggle_imd_legend",
+                  style = "font-size: 80%",
+                  span(class = "glyphicon glyphicon-circle-arrow-up", "Hide")
+                )
+              ),
+              div(
+                id = "imd_legend",
+                tags$div(title = "Index of Multiple Deprivation",
+                         plotOutput(
+                           "imd_legend", width = "100%", height = 180
+                         ))
               )
-            ),
-            div(
-              id = "imd_legend",
-              tags$div(title = "Index of Multiple Deprivation",
-                       plotOutput(
-                         "imd_legend", width = "100%", height = 180
-                       ))
             )
-          )
-        ),
-        tags$div(id = "cite",
-                 htmlOutput("cite_html"))
-      )
-    ),
+          ),
+          tags$div(id = "cite",
+                   htmlOutput("cite_html"))
+        )
+      ),
 
-    tabPanel("Region stats",
-             htmlOutput("region_stats")),
-    tabPanel("Region data",
-             htmlOutput("download_region_current")),
-    tabPanel("National data",
-             htmlOutput("download_national_current")),
-    tabPanel("Manual",
-             includeHTML(file.path("../tabs/manual_body.html"))),
-    tabPanel("About",
-             includeHTML(file.path("../tabs/about_body.html")))
+      tabPanel("Region stats",
+               htmlOutput("region_stats")),
+      tabPanel("Region data",
+               htmlOutput("download_region_current")),
+      tabPanel("National data",
+               htmlOutput("download_national_current")),
+      tabPanel("Manual",
+               includeHTML(file.path("../tabs/manual_body.html"))),
+      tabPanel("About",
+               includeHTML(file.path("../tabs/about_body.html")))
+    )
   )
-)
+}
