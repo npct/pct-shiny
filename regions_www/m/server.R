52aa6e653d0ae79c03375d4c17009e80fe7c542b
Nikolai B <nikolai-b@users.noreply.github.com>
2019-06-27 22:12:52 +0100

    Limit loaded regions by size
    Fixes #746

--- regions_www/m/server.R
+++ regions_www/m/server.R
@@ -239,8 +239,7 @@ shinyServer(function(input, output, session) {
 
   load_data <- function(base_path, filename, purpose, str_lines = NULL){
     filepath <- file.path(base_path, filename)
-
-    while (length(loaded_data) > 100) { # Rm objects from the list when too many (by time last accessed)
+    while (format(object.size(loaded_data), units = "Gb") > 3 ) { # Rm objects (by time last accessed) if the list size is more than 3 Gb
       loaded_data[[names(loaded_data_accessed[loaded_data_accessed == min(unlist(loaded_data_accessed))])]] <<- NULL
     }
     if (file.exists(filepath)) {
